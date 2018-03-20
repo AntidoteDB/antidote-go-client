ec1289664ecf27e1ebf52b8d11bd8997355bc6b7
Mathias Weber <m_weber@cs.uni-kl.de>
2018-03-20 14:57:28 +0100

    set test

--- antidoteclient_test.go
+++ antidoteclient_test.go
@@ -18,15 +18,51 @@ func TestSimple(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	crdtType := CRDTType_COUNTER
+	crdtType := CRDTType_ORSET
 	key := &ApbBoundObject{
 		Bucket: []byte("bucket"),
-		Key:    []byte("key"),
+		Key:    []byte("keySet"),
 		Type:   &crdtType}
-	one := int64(1)
+	addType := ApbSetUpdate_ADD
 	tx.Update(&ApbUpdateOp{
 		Boundobject: key,
-		Operation:   &ApbUpdateOperation{Counterop: &ApbCounterUpdate{Inc: &one}},
+		Operation:   &ApbUpdateOperation{Setop: &ApbSetUpdate{Optype: &addType, Adds: [][]byte{[]byte("test1")}}},
+	})
+	resp, err := tx.Read(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Print(resp.Objects[0])
+
+	_, err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.Close()
+}
+
+func TestSetUpdate(t *testing.T) {
+
+	client, err := NewClient(Host{"127.0.0.1", 8087})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx, err := client.StartTransaction()
+	if err != nil {
+		t.Fatal(err)
+	}
+	crdtType := CRDTType_ORSET
+	key := &ApbBoundObject{
+		Bucket: []byte("bucket"),
+		Key:    []byte("keySet"),
+		Type:   &crdtType}
+	addType := ApbSetUpdate_ADD
+	tx.Update(&ApbUpdateOp{
+		Boundobject: key,
+		Operation:   &ApbUpdateOperation{Setop: &ApbSetUpdate{Optype: &addType, Adds: [][]byte{[]byte("test1")}}},
 	})
 	resp, err := tx.Read(key)
 	if err != nil {
