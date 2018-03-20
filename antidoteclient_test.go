3dd2550674ba1e3e58d41754a606d7d7347912dd
Mathias Weber <m_weber@cs.uni-kl.de>
2018-03-20 10:17:09 +0100

    mass update test

--- antidoteclient_test.go
+++ antidoteclient_test.go
@@ -4,6 +4,7 @@ package antidoteclient
 import (
 	"testing"
 	"fmt"
+	"sync"
 )
 
 func TestSimple(t *testing.T) {
@@ -39,6 +40,56 @@ func TestSimple(t *testing.T) {
 		t.Fatal(err)
 	}
 
+}
+
+func TestManyUpdates(t *testing.T) {
+	client, err := NewClient(Host{"127.0.0.1", 8087})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	crdtType := CRDTType_COUNTER
+	key := &ApbBoundObject{
+		Bucket: []byte("bucket"),
+		Key:    []byte("keyMany"),
+		Type:   &crdtType}
+	one := int64(1)
+
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for k:=0; k<10; k++ {
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				tx, err := client.StartTransaction()
+				if err != nil {
+					t.Fatal(err)
+				}
+				_, err = tx.Update(&ApbUpdateOp{
+					Boundobject: key,
+					Operation:   &ApbUpdateOperation{Counterop: &ApbCounterUpdate{Inc: &one}},
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+				_, err = tx.Commit()
+				if err != nil {
+					t.Fatal(err)
+				}
+				if i%1000 == 0 {
+					fmt.Println(i)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+
+	resp, err := client.StaticRead(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Print(resp.Objects.Objects[0])
 }
 
 func TestStatic(t *testing.T) {
